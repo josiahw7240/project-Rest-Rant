58959e7a8ab4c1b949ca5af71082e1c295124621
josiahw7240 <71360953+josiahw7240@users.noreply.github.com>
2022-08-23 18:49:29 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img src="/images/sleeping-cat.jpg" alt="Sleeping Cat" height="700" width="700" />
+                <div>
+                  Photo by <a href="https://unsplash.com/@modcatshop">ModCatShop</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
+              </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,16 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
-          </main>
+              <div>
+                <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" height="700" width="700" />
+                <div>
+                  Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> <a href="https://unsplash.com/">Unsplash</a>
+                </div>
+              </div>
           <a href="/places">
              <button className="btn-primary">Places Page</button>
           </a>
+          </main>
       </Def>
     )
   }
